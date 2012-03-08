7e23b785aa4f87dc612f488718346af5c2a97416
Vytis Valentinavičius <xytis2000@gmail.com>
2012-03-08 10:49:01 +0000

    On to task 4, step 2.4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
     params[:sort_by] = nil unless params[:sort_by] == 'title' or params[:sort_by] == 'release_date'
     @highlight = params[:sort_by]
     @all_ratings = Movie.ratings
-    @ratings = params[:ratings].keys
+    #@ratings = params[:ratings].keys
     @movies = Movie.find(:all, :order => params[:sort_by])
   end
 
